06a7f0e471aeaee22e469d1eaaeec083c0a5e2b8
Vincent Daranyi <vincent@daranyi.com>
2015-01-24 16:08:49 -0500

    Front page working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Include libraries
-var express = require('express'); // Web server
-var morgan = require('morgan');   // Middleware
+var express = require('express'); 	// Web server
+var morgan = require('morgan');   	// Middleware
+var swig = require('swig'); 		// Html templating
+var routes = require('./routes/'); 	// Reference index.js in routes folder	
+
 
 // Initiate server instance
 var app = express();
@@ -8,11 +11,19 @@ var app = express();
 // Connect Morgan Middleware to server
 app.use(morgan('dev'));
 
-// Simple route
-app.get('/tweet', function(req, res) {
-	res.send('Hello World!');
-});
 
+// Include Swig as html rendering machine
+// WHY do we need to set app engine when we didn't have to do this with EJS?
+app.engine('html', swig.renderFile); 
+app.set('view engine', 'html');
+app.set('views', './views'); 
+swig.setDefaults({ cache: false });
+
+// Answer client requests by running with routes/index.js
+app.use('/', routes);
+
+// Create route to static 'public' folder
+app.use(express.static('public'));
 
 // Make server listen at specific port
 var port = 1234;
---
+++ tweetBank.js
@@ -0,0 +1,19 @@
+var _ = require('underscore');
+
+var data = [];	
+
+var add = function (name, text) {
+  data.push({ name: name, text: text });
+};
+
+var list = function () {
+// WHAT is the purpose of this function?
+  return _.clone(data);
+};
+
+var find = function (properties) {
+  return _.where(data, properties);
+};
+
+module.exports = { add: add, list: list, find: find };
+
